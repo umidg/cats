4342f0e2cfc3c813cc7502676f8ef09d0c8e414c
umidg <70068055+warlord9808@users.noreply.github.com>
2021-11-04 02:02:16 +0545

    initial commit

--- src/pages/container.js
+++ src/pages/container.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
+import TableComponent from "../components/tablecomponent";
 import { callApi } from "../api/index";
 import { USER_API } from "../constants/index";
 import { Sort } from "../utils/sort";
 import { GetRandom } from "../utils/random";
-import TableComponent from "../components/tablecomponent";
+
 
 const Container = () => {
   const [state, setState] = useState({

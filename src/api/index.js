a30415dfbabeef23e9d14c3409770e517543a5e4
umidg <70068055+warlord9808@users.noreply.github.com>
2021-11-04 01:47:15 +0545

    initial commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,11 @@
+import axios from "axios";
 
+export const callApi = async (url, method = "GET") => {
+  const config = {
+    method,
+    url,
+  };
+  return axios(config)
+    .then((res) => res.data)
+    .catch((err) => err);
+};

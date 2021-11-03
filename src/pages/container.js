ba2a1707a88e840209137f7d674e824932186c1c
umidg <70068055+warlord9808@users.noreply.github.com>
2021-11-04 01:45:45 +0545

    initial commit

--- src/pages/container.js
+++ src/pages/container.js
@@ -1,1 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { callApi } from "../api/index";
+import { USER_API } from "../constants/index";
+import { Sort } from "../utils/sort";
+import { GetRandom } from "../utils/random";
+import TableComponent from "../components/tablecomponent";
 
+const Container = () => {
+  const [state, setState] = useState({
+    apiData: [],
+    firstNameData: [],
+  });
+
+  useEffect(() => {
+    callApi(USER_API).then((data) => {
+      const names = [];
+      const changedData = data.map((node) => {
+        names.push(node.firstName);
+        return {
+          age: GetRandom(5, 1),
+          firstName: node.firstName,
+          ninjaLevel: GetRandom(100, 0),
+        };
+      });
+      changedData.sort((a, b) =>
+        Sort(
+          Number(a.ninjaLevel),
+          Number(b.ninjaLevel),
+          "DESC",
+          a.firstName,
+          b.firstName
+        )
+      );
+
+      setState({ ...state, firstNameData: names, apiData: changedData });
+    });
+  }, []);
+  return (
+    <>
+      {state.apiData.length ? (
+        <TableComponent data={state.apiData} />
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </>
+  );
+};
+
+export default Container;

c7bb0f4679619471dd0a7e72204ade2362dc7022
umidg <70068055+warlord9808@users.noreply.github.com>
2021-11-04 02:01:38 +0545

    initial commit

--- src/pages/container.js
+++ src/pages/container.js
@@ -12,28 +12,30 @@ const Container = () => {
   });
 
   useEffect(() => {
-    callApi(USER_API).then((data) => {
-      const names = [];
-      const changedData = data.map((node) => {
-        names.push(node.firstName);
-        return {
-          age: GetRandom(5, 1),
-          firstName: node.firstName,
-          ninjaLevel: GetRandom(100, 0),
-        };
-      });
-      changedData.sort((a, b) =>
-        Sort(
-          Number(a.ninjaLevel),
-          Number(b.ninjaLevel),
-          "DESC",
-          a.firstName,
-          b.firstName
-        )
-      );
+    callApi(USER_API)
+      .then((data) => {
+        const names = [];
+        const changedData = data.map((node) => {
+          names.push(node.firstName);
+          return {
+            age: GetRandom(5, 1),
+            firstName: node.firstName,
+            ninjaLevel: GetRandom(100, 0),
+          };
+        });
+        changedData.sort((a, b) =>
+          Sort(
+            Number(a.ninjaLevel),
+            Number(b.ninjaLevel),
+            "DESC",
+            a.firstName,
+            b.firstName
+          )
+        );
 
-      setState({ ...state, firstNameData: names, apiData: changedData });
-    });
+        setState({ ...state, firstNameData: names, apiData: changedData });
+      })
+      .catch((err) => console.log(err));
   }, []);
   return (
     <>
